7411dc7a65a07456abfe16b60544f32f54f231d1
duckduckcody <cody.hennighan@gmail.com>
2021-04-21 13:11:21 +0800

    make cards bigger

--- src/components/ClotheCardList/ClotheCardList.styles.tsx
+++ src/components/ClotheCardList/ClotheCardList.styles.tsx
@@ -3,13 +3,13 @@ import { MOBILE_BREAKPOINT } from '../../constants';
 
 export const ClotheCardListContainer = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, 200px);
-  grid-auto-rows: 400px;
+  grid-template-columns: repeat(auto-fill, 400px);
+  grid-auto-rows: 700px;
   gap: 2rem;
   justify-content: center;
 
   @media (max-width: ${MOBILE_BREAKPOINT}) {
     grid-template-columns: 1fr 1fr;
-    grid-auto-rows: 300px;
+    grid-auto-rows: 350px;
   } ;
 `;
