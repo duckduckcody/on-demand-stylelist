342281c0be463888f4c7e04686fe146b3e98c1f6
duckduckcody <cody.hennighan@gmail.com>
2021-06-27 15:51:49 +0800

    more complex json parsing

--- src/client/util/swrFetcher.ts
+++ src/client/util/swrFetcher.ts
@@ -11,27 +11,33 @@ export const swrFetcher = async <JSON = unknown>(
 
   if (!res.ok) {
     res
-      .json()
-      .then((json) => {
-        console.log('inside json');
-        console.log('res', res);
-        const error: FetcherError = {
-          message: json?.message ?? 'A server error has occurred',
-          status: res.status,
-          id: json?.id,
-        };
-        // throw error;
-        Promise.reject(error);
+      .text()
+      .then((text) => {
+        try {
+          const json = JSON.parse(text);
+          console.log('below parse');
+          const error: FetcherError = {
+            message: json?.message ?? 'A server error has occurred',
+            status: res.status,
+            id: json?.id,
+          };
+          throw error;
+        } catch (e) {
+          console.log('in catch');
+          const error: FetcherError = {
+            message: 'A server error has occurred',
+            status: res.status,
+          };
+          throw error;
+        }
       })
       .catch(() => {
-        console.log('inside caught');
-        console.log('res', res);
+        console.log('in promise catch');
         const error: FetcherError = {
           message: 'A server error has occurred',
-          status: res.status,
+          status: 500,
         };
-        // throw error;
-        Promise.reject(error);
+        throw error;
       });
   }
 
